f6dd9c23665bd3c1ab40b9e4e961722ad92b79ac
MassiPozzuto <massipozzuto2016@gmail.com>
2023-05-03 19:54:25 -0300

    agrego index y arreglo cosas del css

--- views/layout.php
+++ views/layout.php
@@ -8,7 +8,15 @@
     <!-- JQUERY -->
     <script src="../js/jquery.min.js"></script>
     <!-- CSS -->
-    <link rel="stylesheet" href="../css/login_register.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <?php
+    if ($view == "login" || $view == "register") { ?>
+        <link rel="stylesheet" href="../css/login_register.css">
+    <?php
+    } elseif ($view == "home") { ?>
+        <link rel="stylesheet" href="../css/home.css">
+    <?php
+    } ?>
     <title><?php echo $title ?> - Jotelson</title>
 </head>
 
