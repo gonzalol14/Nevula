c76f34a023a924b1c7550d2869b6ebb7a1de872a
MassiPozzuto <massipozzuto2016@gmail.com>
2023-05-04 02:24:03 -0300

    agrego navbar

--- views/home.php
+++ views/home.php
@@ -1,4 +1,26 @@
 <nav>
+    <div class="container__left">
+        <div class="container__logo">
+            <img src="../img/logo_empresa_jotelson.png" alt="Logo empresa" width="65px">
+        </div>
+        
+        <div class="container__list-buttons">
+            <div>
+                <button>INICIO</button>
+            </div>
+            <div>
+                <button>NOVEDADES</button>
+            </div>
+            <div>
+                <button>COMUNIDAD</button>
+            </div>
+            <div>
+                <button>AYUDA</button>
+            </div>
+        </div>
+    </div>
 
+    <div class="container__download">
+        <button>DESCARGAR</button>
+    </div>
 </nav>
-
