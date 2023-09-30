2508ebb5d821f14ead8134438c9b92b6ccdd7108
MassiPozzuto <massipozzuto2016@gmail.com>
2023-09-30 00:44:23 -0300

    Agrego migracion para hacer unicas la columna Email y Username

--- C#_Nevula/NevulaForo/Migrations/20230930033700_ColumnaUnicaEmailyUsername.Designer.cs
+++ C#_Nevula/NevulaForo/Migrations/20230930033700_ColumnaUnicaEmailyUsername.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NevulaForo.Models.DB;
 
@@ -11,9 +12,11 @@ using NevulaForo.Models.DB;
 namespace NevulaForo.Migrations
 {
     [DbContext(typeof(NevulaContext))]
-    partial class NevulaContextModelSnapshot : ModelSnapshot
+    [Migration("20230930033700_ColumnaUnicaEmailyUsername")]
+    partial class ColumnaUnicaEmailyUsername
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ C#_Nevula/NevulaForo/Migrations/20230930033700_ColumnaUnicaEmailyUsername.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NevulaForo.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnaUnicaEmailyUsername : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "role",
+                newName: "Role");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "User",
+                type: "varchar(100)",
+                unicode: false,
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+
+            migrationBuilder.CreateIndex(
+                name: "email",
+                table: "User",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "username",
+                table: "User",
+                column: "username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Role",
+                newName: "role");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "User",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldUnicode: false,
+                oldMaxLength: 100);
+        }
+    }
+}
