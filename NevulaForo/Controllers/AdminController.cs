82e413010ac309510d10ea338afc8d9d9ce3f776
MassiPozzuto <massipozzuto2016@gmail.com>
2023-11-09 19:39:03 -0300

    Agrego pagina admin users sin funcionalidades

--- NevulaForo/Controllers/AdminController.cs
+++ NevulaForo/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NevulaForo.Models.DB;
+using System.Security.Claims;
 
 namespace NevulaForo.Controllers
 {
@@ -8,17 +11,42 @@ namespace NevulaForo.Controllers
     [Authorize(Policy = "PolicyAdministrator")]
     public class AdminController : Controller
     {
-        //PAGINAS
-        public ActionResult Users()
+        private readonly NevulaContext _DBContext;
+
+        public AdminController(NevulaContext dbContext)
         {
-            return View();
+            _DBContext = dbContext;
         }
-        public ActionResult Publications()
+
+        //PAGINAS
+        public IActionResult Users()
+        {
+            ClaimsPrincipal claimUser = HttpContext.User;
+            int IdUser = Convert.ToInt32(claimUser.Claims.Where(c => c.Type == "Id").Select(c => c.Value).SingleOrDefault());
+
+            List<User> accounts = _DBContext.Users
+                                    .Include(u => u.UserRoles)
+                                    .Where(u => u.DeletedAt == null && u.Id != IdUser)
+                                    .Select(u => new User
+                                    {
+                                        Id = u.Id,
+                                        Name = u.Name,
+                                        Surname = u.Surname,
+                                        Username = u.Username,
+                                        Description = u.Description,
+                                        CreatedAt = u.CreatedAt,
+                                        Publications = u.Publications.Where(post => post.DeletedAt == null && post.IdUserNavigation.DeletedAt == null).ToList(),
+                                        UserRoles = u.UserRoles
+                                    })
+                                    .ToList();
+            return View(accounts);
+        }
+        public IActionResult Publications()
         {
             return View();
         }
 
-        public ActionResult Comments()
+        public IActionResult Comments()
         {
             return View();
         }
@@ -27,19 +55,19 @@ namespace NevulaForo.Controllers
         //Acciones
         [HttpPost]
         //Deben enviar mails al usuario al que se le borra su cuenta, su publicacion o su comentario
-        public ActionResult DeleteUser()
+        public IActionResult DeleteUser()
         {
             return View();
         } 
 
         [HttpPost]
-        public ActionResult DeletePublication()
+        public IActionResult DeletePublication()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult DeleteComment()
+        public IActionResult DeleteComment()
         {
             return View();
         }
