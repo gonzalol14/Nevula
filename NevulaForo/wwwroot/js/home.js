5926befdc7bba3aa513690c2956079913a3a3bc1
MassiPozzuto <massipozzuto2016@gmail.com>
2023-11-16 20:55:49 -0300

    Agrego footer y parte de pagina de inicio

---
+++ NevulaForo/wwwroot/js/home.js
@@ -0,0 +1,10 @@
+﻿document.getElementById("btn_back-top").addEventListener("click", function () {
+    const scrollStep = -window.scrollY / (500 / 15); // Ajusta la velocidad aquí
+    const scrollInterval = setInterval(function () {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, 15);
+});
